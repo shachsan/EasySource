cb37883992ce2785655380a436fc2b08072226d5
Sanjay Sai <shachsan@hotmail.com>
2018-12-22 19:23:51 -0500

    work on products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+    before_action :find_product, only:[:show]
 
     def index
         @products = Product.all 
@@ -6,7 +7,16 @@ class Api::V1::ProductsController < ApplicationController
     end
 
     def show
+        render json: @product
+    end 
 
+    private
+    def product_params
+        params.permit(:barcode, :name, :size, :case_pack, :brand, :category_id, :img_url)
+    end
+
+    def find_product
+        @product = Product.find(params[:id])
     end 
 
 end
