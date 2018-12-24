e5e3918e0afe9c0043b1471ff9f52097741fd9ff
Sanjay Sai <shachsan@hotmail.com>
2018-12-24 01:02:24 -0500

    fix routes for categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :vendors, :products
+      resources :vendors, :products, :categories
     end
   end
 end
