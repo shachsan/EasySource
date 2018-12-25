a01a1ff5a282f08afd00f27f92113c9fb2d2e2f8
Sanjay Sai <shachsan@hotmail.com>
2018-12-25 11:23:44 -0500

    everything works until this commit

--- app/serializers/vendor_serializer.rb
+++ app/serializers/vendor_serializer.rb
@@ -1,12 +1,13 @@
 class VendorSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :email, :has_min, :min_amount
   # :has_min, :min_amount, :price, :inventory
-  # has_many :products
-  has_many :vendor_products
   has_many :products
+  # has_many :vendor_products, include: :products
+  has_many :vendor_products
 
   # def products
-  #   object.vendor_products.case_price
+  #   # object.products
+  #   object.vendor_products
   # end
   
 
