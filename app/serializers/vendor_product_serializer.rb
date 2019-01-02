682402bf32367c412e6576462b2652ed654baf20
Sanjay Sai <shachsan@hotmail.com>
2019-01-02 00:45:37 -0500

    change vp controllers for phone and emai

--- app/serializers/vendor_product_serializer.rb
+++ app/serializers/vendor_product_serializer.rb
@@ -1,9 +1,19 @@
 class VendorProductSerializer < ActiveModel::Serializer
-  attributes :id, :v_item, :case_price, :vendor_name
+  attributes :id, :v_item, :case_price, :phone, :email, :vendor_name
   belongs_to :product
   belongs_to :vendor
 
   def vendor_name
     object.vendor.name #object is current vendor_product object get name from that
+  
+  end
+
+  def phone
+    object.vendor.phone
+  end 
+
+  def email
+    object.vendor.email
+    
   end
 end
